c947303d983ad5a7b2115ff51e6fec968de5f5c7
Jamie Lennox <jamielennox@gmail.com>
2012-05-20 17:49:40 +1000

    Remove hacks trying to use deluge library

--- addon.py
+++ addon.py
@@ -4,23 +4,9 @@ import urllib
 
 import xbmcaddon
 
-import platform
-import deluge.ui.common
-
-try: 
-    system = platform.system()
-except IOError: 
-    print "IO Error getting platform" 
-    
-    def fake_windows_check(): 
-        print "*** FAKE WINDOWS CHECK ***" 
-        return sys.platform.startswith('win')
-
-    deluge.common.windows_check = fake_windows_check
-
-__scriptname__ = "XBMC-Deluge" 
-__author__ = "Jamie Lennox <jamielennox@gmail.com>" 
-__version__ = "0.1" 
+__scriptname__ = "XBMC-Deluge"
+__author__ = "Jamie Lennox <jamielennox@gmail.com>"
+__version__ = "0.1"
 
 __settings__ = xbmcaddon.Addon(id='script.deluge')
 __language__ = __settings__.getLocalizedString
